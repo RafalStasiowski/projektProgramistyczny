bf8dbcba654409f9dbc3956bd857f949089cdc00
Rafał Stasiowski <39459132+RafalStasiowski@users.noreply.github.com>
2022-05-22 10:47:58 +0200

    Created basic functionality of face anonymize

---
+++ find_face.py
@@ -0,0 +1,37 @@
+import cv2
+from anonymize_face import anonymize_face_simple
+from anonymize_face import anonymize_face_pixelate
+import numpy as np
+
+
+def find_face(image):
+    prototxtPath = "face_detector/deploy.prototxt"
+    weightsPath = "face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+    confidence_in = 0.2
+    # prototxtPath = 'face_detector/aaa/graph.pbtxt'
+    # weightsPath = 'face_detector/aaa/frozen_inference_graph.pb'
+
+    (h, w) = image.shape[:2]
+
+    net = cv2.dnn.readNet(prototxtPath, weightsPath)
+    blob = cv2.dnn.blobFromImage(image, 1.0, (1024, 1024),
+                                 (104.0, 177.0, 123.0))
+    net.setInput(blob)
+    detections = net.forward()
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > confidence_in:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            face = image[startY:endY, startX:endX]
+
+            face = anonymize_face_pixelate(face, blocks=3)
+
+            image[startY:endY, startX:endX] = face
+
+    output = image
+    return output
+
+
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from flask import Flask
+import flask
+from flask import Flask, render_template, Blueprint
+import cv2
+from anonymize_face import anonymize_face_pixelate
+from anonymize_face import anonymize_face_simple
+from find_face import find_face
 
 app = Flask(__name__)
+mod = Blueprint('', __name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
 
-def welcome():
-    return "Hello World!"
+@app.route('/', methods=['GET'])
+def new_checkout():
+    return render_template('index.html')
+
+
+@app.route('/image-upload/', methods=['GET', 'POST'])
+def upload():
+    imagefile = flask.request.files.get('imagefile', '')
+    imagefile = cv2.imread('human.jpg')
+    image = find_face(imagefile)
+    return flask.send_file(image, as_attachment=True, mimetype='image/jpg', download_name='aaa')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
